018fe6b60784e122269431b3838c06caa58fa4ca
MARK GRIMES - Student <G00289916@gmit.ie>
2016-10-05 10:18:16 +0100

    Changed to post method

--- hello.py
+++ hello.py
@@ -1,17 +1,22 @@
 from flask import Flask
-from flask import request
+from flask import request,url_for,redirect
 app = Flask(__name__)
 
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return redirect (url_for('static', filename='index.html'))
+	#redirects the user to the index
+	#"static" means it doesnt change and is in the static folder
 	
-@app.route("/name/",methods=["GET"])
+@app.route("/name",methods=["Post"])
 def name():
-	yourName=request.args.get("name");
-	return "Your name is: %s" %yourName
+	if request.method == "POST":
+		yourName=request.form["name"];
+		return "Your name is: %s" %yourName
+	else:
+		return "Errorrrr"
 
 if __name__ == "__main__":
     app.run()
