fd8663551dd40f157b431b5f68ca474f035f1880
MARK GRIMES - Student <G00289916@gmit.ie>
2016-10-18 15:00:00 +0100

    ajax call

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
-from flask import Flask
-from flask import request,url_for,redirect
-app = Flask(__name__)
+import flask as fl
+import itertools as it
+app = fl.Flask(__name__)
 
 
 
 @app.route("/")
 def hello():
-    return redirect (url_for('static', filename='index.html'))
+    return app.send_static_file("index.html")
 	#redirects the user to the index
 	#"static" means it doesnt change and is in the static folder
 	
@@ -17,6 +17,11 @@ def name():
 		return "Your name is: %s" %yourName
 	else:
 		return "Errorrrr"
+
+@app.route("/perms/")
+def perms():
+	perms = [''.join(p) for p in it.permutations(fl.request.args.get("userinput"))]
+	return '\n'.join(perms)
 
 if __name__ == "__main__":
     app.run()
